2e809c327f8c24c6673b235eb1cf55bd91d35df6
Orewaluffy4 <achupradeesh1984@gmail.com>
2025-04-09 13:53:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,32 +2,19 @@
 #include "blockchain.h"
 
 int main() {
-    Blockchain myCoin;
+    Blockchain myCoin(4); // Set difficulty
 
-    std::cout << "Mining block 1..." << std::endl;
-    myCoin.addTransaction(Transaction("Alice", "Bob", 10));
-    myCoin.minePendingTransactions("Miner1");
+    myCoin.addBlock("First Block");
+    myCoin.addBlock("Second Block");
 
-    std::cout << "Balance of Miner1: " << myCoin.getBalanceOfAddress("Miner1") << std::endl;
-
-    std::cout << "Mining block 2..." << std::endl;
-    myCoin.addTransaction(Transaction("Bob", "Charlie", 5));
-    myCoin.minePendingTransactions("Miner1");
-
-    std::cout << "Balance of Miner1: " << myCoin.getBalanceOfAddress("Miner1") << std::endl;
-
-    std::cout << "Blockchain:" << std::endl;
-    for (const auto& block : myCoin.getChain()) {
-        std::cout << "------------------------" << std::endl;
+    for (const Block& block : myCoin.getChain()) {
         std::cout << "Index: " << block.getIndex() << std::endl;
         std::cout << "Timestamp: " << block.getTimestamp() << std::endl;
         std::cout << "Hash: " << block.getHash() << std::endl;
         std::cout << "Previous Hash: " << block.getPreviousHash() << std::endl;
         std::cout << "Nonce: " << block.getNonce() << std::endl;
-        std::cout << "Data: " << std::endl;
-        for (const auto& d : block.getData()) {
-            std::cout << "  " << d << std::endl;
-        }
+        std::cout << "Data: " << block.getData() << std::endl;
+        std::cout << "----------------------------" << std::endl;
     }
 
     return 0;
