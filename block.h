79502c2ed5c5b29561cf651013b8de432913574a
Orewaluffy4 <achupradeesh1984@gmail.com>
2025-04-09 13:43:10 +0530

    Update block.h

--- block.h
+++ block.h
@@ -1,24 +1,16 @@
-#ifndef BLOCK_H
-#define BLOCK_H
-
-#include <vector>
+#pragma once
 #include <string>
-#include <ctime>
+#include <vector>
 #include "transaction.h"
 
 class Block {
 public:
     int index;
-    std::time_t timestamp;
+    std::string timestamp;
     std::vector<Transaction> transactions;
     std::string previousHash;
     std::string hash;
-    uint64_t nonce;
 
     Block(int idx, const std::vector<Transaction>& txs, const std::string& prevHash);
-
     std::string calculateHash() const;
-    void mineBlock(uint32_t difficulty);
 };
-
-#endif
